cd24a44555bfc17c5c30ba0c7c88f4c41748e8d4
Bl00dyB0dy <stepnova_marina@bk.ru>
2024-02-11 18:17:05 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,6 @@ class Main {
         int Us_min;
         Us_max = Math.max(Math.max(a, b), Math.max(c, b));
         Us_min = Math.min(Math.min(a, b), Math.min(c, b));
-        System.out.println(Us_max + " " + Us_min + " ");
+        System.out.println(Us_max + " " + Us_min);
     }
 }
